998e64b471333c56c4619afc0619bd27e215ae91
UĞUR DUYMUŞ <ugurdymss@gmail.com>
2023-03-16 20:57:19 +0300

    Otobüs Bilet Satış Otomasyonu
    C# ile Otobüs Bilet Satış Otomasyonu

---
+++ Otobüsbiletsatışotomasyon/Form1.Designer.cs
@@ -0,0 +1,493 @@
+﻿namespace Otobüsbiletsatışotomasyon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader6 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader7 = new System.Windows.Forms.ColumnHeader();
+            this.columnHeader8 = new System.Windows.Forms.ColumnHeader();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbOtobus = new System.Windows.Forms.ComboBox();
+            this.cmbNereden = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbNereye = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpTarih = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nudFiyat = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.rezerveEtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.nudFiyat)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6,
+            this.columnHeader7,
+            this.columnHeader8});
+            this.listView1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.listView1.GridLines = true;
+            this.listView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listView1.Location = new System.Drawing.Point(668, 6);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(764, 621);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Müşteri";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Telefon";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Cinsiyet";
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Nereden";
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Nereye";
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Koltuk No";
+            // 
+            // columnHeader7
+            // 
+            this.columnHeader7.Text = "Tarih";
+            // 
+            // columnHeader8
+            // 
+            this.columnHeader8.Text = "Fiyat";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(269, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Otobüs Seçiniz";
+            // 
+            // cmbOtobus
+            // 
+            this.cmbOtobus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOtobus.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cmbOtobus.FormattingEnabled = true;
+            this.cmbOtobus.Items.AddRange(new object[] {
+            "Travego",
+            "Neoplan",
+            "Setra"});
+            this.cmbOtobus.Location = new System.Drawing.Point(385, 41);
+            this.cmbOtobus.Name = "cmbOtobus";
+            this.cmbOtobus.Size = new System.Drawing.Size(151, 24);
+            this.cmbOtobus.TabIndex = 2;
+            this.cmbOtobus.SelectedIndexChanged += new System.EventHandler(this.cmbOtobus_SelectedIndexChanged);
+            // 
+            // cmbNereden
+            // 
+            this.cmbNereden.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbNereden.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cmbNereden.FormattingEnabled = true;
+            this.cmbNereden.Items.AddRange(new object[] {
+            "Adana",
+            "Adıyaman",
+            "Afyonkarahisar",
+            "Ağrı",
+            "Aksaray",
+            "Amasya",
+            "Ankara",
+            "Antalya",
+            " Ardahan",
+            "Artvin",
+            "Aydın",
+            "Balıkesir",
+            "Bartın",
+            ",Batman",
+            "Bayburt",
+            "Bilecik",
+            "Bingöl",
+            "Bitlis",
+            "Bolu",
+            "Burdur",
+            "Bursa",
+            "Çanakkale",
+            "Çankırı",
+            "Çorum",
+            "Denizli",
+            "Diyarbakır",
+            "Düzce",
+            "Edirne",
+            "Elazığ",
+            "Erzincan",
+            "Erzurum",
+            "Eskişehir",
+            "Gaziantep",
+            "Giresun",
+            "Gümüşhane",
+            "Hakkâri",
+            "Hatay",
+            "Iğdır",
+            "Isparta",
+            "İstanbul",
+            "İzmir",
+            "Kahramanmaraş",
+            "Karabük",
+            "Karaman",
+            "Kars",
+            "Kastamonu",
+            "Kayseri",
+            "Kilis",
+            "Kırıkkale",
+            "Kırklareli",
+            "Kırşehir",
+            "Kocaeli",
+            "Konya",
+            "Kütahya",
+            "Malatya",
+            "Manisa",
+            "Mardin",
+            "Mersin",
+            "Muğla",
+            "Muş",
+            "Nevşehir",
+            "Niğde",
+            "Ordu",
+            "Osmaniye",
+            "Rize",
+            "Sakarya",
+            "Samsun",
+            "Şanlıurfa",
+            "Siirt",
+            "Sinop",
+            "Sivas",
+            "Şırnak",
+            "Tekirdağ",
+            "Tokat",
+            "Trabzon",
+            "Tunceli",
+            "Uşak",
+            "Van",
+            "Yalova",
+            "Yozgat",
+            "Zonguldak"});
+            this.cmbNereden.Location = new System.Drawing.Point(79, 36);
+            this.cmbNereden.Name = "cmbNereden";
+            this.cmbNereden.Size = new System.Drawing.Size(151, 24);
+            this.cmbNereden.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(4, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nereden";
+            // 
+            // cmbNereye
+            // 
+            this.cmbNereye.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbNereye.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.cmbNereye.FormattingEnabled = true;
+            this.cmbNereye.Items.AddRange(new object[] {
+            "Adana",
+            "Adıyaman",
+            "Afyonkarahisar",
+            "Ağrı",
+            "Aksaray",
+            "Amasya",
+            "Ankara",
+            "Antalya",
+            " Ardahan",
+            "Artvin",
+            "Aydın",
+            "Balıkesir",
+            "Bartın",
+            ",Batman",
+            "Bayburt",
+            "Bilecik",
+            "Bingöl",
+            "Bitlis",
+            "Bolu",
+            "Burdur",
+            "Bursa",
+            "Çanakkale",
+            "Çankırı",
+            "Çorum",
+            "Denizli",
+            "Diyarbakır",
+            "Düzce",
+            "Edirne",
+            "Elazığ",
+            "Erzincan",
+            "Erzurum",
+            "Eskişehir",
+            "Gaziantep",
+            "Giresun",
+            "Gümüşhane",
+            "Hakkâri",
+            "Hatay",
+            "Iğdır",
+            "Isparta",
+            "İstanbul",
+            "İzmir",
+            "Kahramanmaraş",
+            "Karabük",
+            "Karaman",
+            "Kars",
+            "Kastamonu",
+            "Kayseri",
+            "Kilis",
+            "Kırıkkale",
+            "Kırklareli",
+            "Kırşehir",
+            "Kocaeli",
+            "Konya",
+            "Kütahya",
+            "Malatya",
+            "Manisa",
+            "Mardin",
+            "Mersin",
+            "Muğla",
+            "Muş",
+            "Nevşehir",
+            "Niğde",
+            "Ordu",
+            "Osmaniye",
+            "Rize",
+            "Sakarya",
+            "Samsun",
+            "Şanlıurfa",
+            "Siirt",
+            "Sinop",
+            "Sivas",
+            "Şırnak",
+            "Tekirdağ",
+            "Tokat",
+            "Trabzon",
+            "Tunceli",
+            "Uşak",
+            "Van",
+            "Yalova",
+            "Yozgat",
+            "Zonguldak"});
+            this.cmbNereye.Location = new System.Drawing.Point(79, 83);
+            this.cmbNereye.Name = "cmbNereye";
+            this.cmbNereye.Size = new System.Drawing.Size(151, 24);
+            this.cmbNereye.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(4, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nereye";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(310, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Tarih";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dtpTarih
+            // 
+            this.dtpTarih.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dtpTarih.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpTarih.Location = new System.Drawing.Point(385, 201);
+            this.dtpTarih.Name = "dtpTarih";
+            this.dtpTarih.Size = new System.Drawing.Size(151, 22);
+            this.dtpTarih.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(312, 235);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Fiyat";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // nudFiyat
+            // 
+            this.nudFiyat.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.nudFiyat.Location = new System.Drawing.Point(386, 233);
+            this.nudFiyat.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.nudFiyat.Minimum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.nudFiyat.Name = "nudFiyat";
+            this.nudFiyat.Size = new System.Drawing.Size(150, 22);
+            this.nudFiyat.TabIndex = 10;
+            this.nudFiyat.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(340, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 34);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "KAYDET";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbNereden);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.cmbNereye);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(298, 71);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(238, 125);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "İSTİKAMET";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.rezerveEtToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(148, 28);
+            // 
+            // rezerveEtToolStripMenuItem
+            // 
+            this.rezerveEtToolStripMenuItem.Name = "rezerveEtToolStripMenuItem";
+            this.rezerveEtToolStripMenuItem.Size = new System.Drawing.Size(147, 24);
+            this.rezerveEtToolStripMenuItem.Text = "Rezerve Et";
+            this.rezerveEtToolStripMenuItem.Click += new System.EventHandler(this.rezerveEtToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1444, 639);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.nudFiyat);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dtpTarih);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cmbOtobus);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudFiyat)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private Label label1;
+        private ComboBox cmbOtobus;
+        private ComboBox cmbNereden;
+        private Label label2;
+        private ComboBox cmbNereye;
+        private Label label3;
+        private Label label4;
+        private DateTimePicker dtpTarih;
+        private Label label5;
+        private NumericUpDown nudFiyat;
+        private Button button1;
+        private GroupBox groupBox1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader7;
+        private ColumnHeader columnHeader8;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem rezerveEtToolStripMenuItem;
+    }
+}
---
+++ Otobüsbiletsatışotomasyon/Form1.cs
@@ -0,0 +1,146 @@
+using System.Globalization;
+using System.Security.Policy;
+
+namespace Otobüsbiletsatışotomasyon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbOtobus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch(cmbOtobus.Text)
+            {
+                case "Trevego":KoltukDoldur(8, false);
+                    break;
+                case "Setra":KoltukDoldur(12, true);
+                    break;
+                case "Neoplan":KoltukDoldur(10, false);
+                    break;  
+            }
+            void KoltukDoldur(int sira,bool arkaBesliMi)
+            {
+                yavaslat:
+                foreach(Control ctrl in this.Controls)
+                {
+                    if(ctrl is Button)
+                    {
+                        Button btn = ctrl as Button;
+                        if(btn.Text=="Kaydet")
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            this.Controls.Remove(ctrl);
+                            goto yavaslat;
+                        }
+                    }
+                }
+                int koltukNo = 1;
+                for(int i=0;i<sira;i++)
+                {
+                    for (int j=0; j<5; j++)
+                    {
+                        if(arkaBesliMi==true)
+                        {
+                            if(i!= sira-1 && j==2)
+                            {
+                                continue;
+                            }
+                                
+                        }
+                        else
+                        {
+                            if (j == 2)
+                                continue;
+                        }
+                        
+                        Button koltuk = new Button();
+                        koltuk.Height = koltuk.Width = 40;
+                        koltuk.Top = 30 + (i * 45);
+                        koltuk.Left = 5 + (j * 45);
+                        koltuk.Text=koltukNo.ToString();
+                        koltukNo++;
+                        koltuk.ContextMenuStrip = contextMenuStrip1;
+                        koltuk.MouseDown += Koltuk_MouseDown;
+                        this.Controls.Add(koltuk);
+
+
+                    }
+                }
+            }
+        }
+        Button tiklanan;
+        private void Koltuk_MouseDown(object? sender, MouseEventArgs e)
+        {
+            tiklanan = sender as Button;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rezerveEtToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if(cmbOtobus.SelectedIndex == -1 || cmbNereden.SelectedIndex == -1 || cmbNereye.SelectedIndex == -1)
+            {
+                MessageBox.Show("Lütfen önce gerekli alanları doldurunuz!");
+                return;
+            }
+            KayıtFormu kf = new KayıtFormu();
+            DialogResult sonuc = kf.ShowDialog();
+            if(sonuc == DialogResult.OK)
+            {
+                ListViewItem lvi= new ListViewItem();
+                lvi.Text = String.Format("{0} {1}", kf.txtIsim.Text, kf.txtSoyisim.Text);
+                lvi.SubItems.Add(kf.mskdTelefon.Text);
+                if(kf.rdbBay.Checked)
+                {
+                    lvi.SubItems.Add("BAY");
+                    tiklanan.BackColor = Color.Blue;
+                }
+                if(kf.rdbBayan.Checked)
+                {
+                    lvi.SubItems.Add("BAYAN");
+                    tiklanan.BackColor = Color.Red;
+                }
+                lvi.SubItems.Add(cmbNereden.Text);
+                lvi.SubItems.Add(cmbNereye.Text);
+                lvi.SubItems.Add(tiklanan.Text);
+                lvi.SubItems.Add(dtpTarih.Text);
+                lvi.SubItems.Add(nudFiyat.Value.ToString());
+                listView1.Items.Add(lvi);
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
